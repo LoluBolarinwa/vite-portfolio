a8bd512a058349063d624c20631e583f7619b523
Lolu Bolarinwa <lolubolarinwa@gmail.com>
2023-10-27 16:23:55 +0100

    commit after vercel production

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -5,23 +5,23 @@ function Projects(){
             <div className="project-parentDiv">
                 <div className="project-1   ">
                     <div className="project-img">
-                        {/* <img></img> */}
+                    <a href="https://www.example.com">
+                        <img className="img-project"  src="/src/assets/rsz_6536f7a0b2700.png"/>
+                    </a>
                     </div>
-                    <div className="project-description">
+                    <div className="project-description"><p>Header </p>
                         <h3>React Dashboard</h3>
-                        <p>Lorem Ipsum jekajade lo si ode. Iwo na mo wipe mo feran re.
-                            Inu mi ma tutu ti o ba sowipe o nbo wa si odo mi.
-                        </p>
+                        <p>Data Dashboard Built using React Functional Components, Props and Material UI.</p>
+                        <a href="https://www.example.com">Click here to visit Example.com</a>
                     </div>
                 </div>
                 <div className="project-1">
                     <div className="project-img">
-                        {/* <img></img> */}
+                    <img className="img-project" src="/src/assets/2 ezgif-5-1ba6ea9c67.gif"/>
                     </div>
                     <div className="project-description">
-                        <h3>Meh</h3>
-                        <p>Lorem Ipsum jekajade lo si ode. Iwo na mo wipe mo feran re.
-                            Inu mi ma tutu ti o ba sowipe o nbo wa si odo mi.
+                        <h3>Dynamic Image Weather App</h3>
+                        <p>Weather app that displays dynamic images based correlating to weather patterns Fullstack weather app. Built using API JavaScript Fetch method.
                         </p>
                     </div>
                 </div>

41e5c57d20c9afab56c8a4cd37d294fce3c525a4
Lolu Bolarinwa <lolubolarinwa@gmail.com>
2023-10-31 00:17:04 +0100

    first commit to new branch

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,58 +1,52 @@
-function Projects(){
-    return(
-        <div id="project-back"> 
-            <div id="proj-cont"><a href="#" id="Portfolio"></a>
-            <div className="project-parentDiv">
-                <div className="project-1   ">
-                    <div className="project-img">
-                        {/* <img></img> */}
-                    </div>
-                    <div className="project-description">
-                        <h3>React Dashboard</h3>
-                        <p>Lorem Ipsum jekajade lo si ode. Iwo na mo wipe mo feran re.
-                            Inu mi ma tutu ti o ba sowipe o nbo wa si odo mi.
-                        </p>
-                    </div>
-                </div>
-                <div className="project-1">
-                    <div className="project-img">
-                        {/* <img></img> */}
-                    </div>
-                    <div className="project-description">
-                        <h3>Meh</h3>
-                        <p>Lorem Ipsum jekajade lo si ode. Iwo na mo wipe mo feran re.
-                            Inu mi ma tutu ti o ba sowipe o nbo wa si odo mi.
-                        </p>
-                    </div>
-                </div>
-                <div className="project-1"> 
-                    <div className="project-img">
-                        {/* <img></img> */}
-                    </div>
-                    <div className="project-description">
-                        <h3 className="rebelh3">Meh</h3>
-                        <p>Lorem Ipsum jekajade lo si ode. Iwo na mo wipe mo feran re.
-                            Inu mi ma tutu ti o ba sowipe o nbo wa si odo mi.
-                        </p>
-                    </div>
-                </div>
-                <div className="project-1">
-                    <div className="project-img">
-                        {/* <img></img> */}
-                    </div>
-                    <div className="project-description">
-                        <h3>Meh</h3>
-                        <p>Lorem Ipsum jekajade lo si ode. Iwo na mo wipe mo feran re.
-                            Inu mi ma tutu ti o ba sowipe o nbo wa si odo mi.
-                        </p>
-                    </div>
-                </div>
-                
-            </div>    
-            </div>
-        </div>
+function Projects() {
+  return (
+    <div id="project-back">
+      <div id="proj-cont">
+        <a href="#" id="Portfolio"></a>
+        <div className="project-parentDiv">
+          <div className="project-1">
+            <a className="img-1" href="https://lolubolarinwa.github.io/dashboard-react" />
 
-    )
+            <div className="project-description">
+              <h3>React Dashboard</h3>
+              <p>A Dashboard built using React Components, Material UI and </p>
+              <p>Inu mi ma tutu ti o ba sowipe o nbo wa si odo mi.</p>
+            </div>
+          </div>
+          <div className="project-1 img-hover">
+            <a className="img-2 " href="https://lolubolarinwa.github.io/weather-app-project-/" />
+            <div className="project-description">
+              <h3>Meh</h3>
+              <p>
+                Lorem Ipsum jekajade lo si ode. Iwo na mo wipe mo feran re. Inu
+                mi ma tutu ti o ba sowipe o nbo wa si odo mi.
+              </p>
+            </div>
+          </div>
+          <div className="project-1">
+            <div className="project-img">{/* <img></img> */}</div>
+            <div className="project-description">
+              <h3 className="rebelh3">Meh</h3>
+              <p>
+                Lorem Ipsum jekajade lo si ode. Iwo na mo wipe mo feran re. Inu
+                mi ma tutu ti o ba sowipe o nbo wa si odo mi.
+              </p>
+            </div>
+          </div>
+          <div className="project-1">
+            <div className="project-img">{/* <img></img> */}</div>
+            <div className="project-description">
+              <h3>Meh</h3>
+              <p>
+                Lorem Ipsum jekajade lo si ode. Iwo na mo wipe mo feran re. Inu
+                mi ma tutu ti o ba sowipe o nbo wa si odo mi.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Projects;

9cd5cd5c0542c621d89076a4d4f00cd986ef46d0
Lolu Bolarinwa <lolubolarinwa@gmail.com>
2023-10-31 02:58:24 +0100

    refined project section

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -5,41 +5,35 @@ function Projects() {
         <a href="#" id="Portfolio"></a>
         <div className="project-parentDiv">
           <div className="project-1">
-            <a className="img-1" href="https://lolubolarinwa.github.io/dashboard-react" />
+            <a className="img-1 img-p" href="https://lolubolarinwa.github.io/dashboard-react" target="_blank"/>
 
             <div className="project-description">
               <h3>React Dashboard</h3>
-              <p>A Dashboard built using React Components, Material UI and </p>
-              <p>Inu mi ma tutu ti o ba sowipe o nbo wa si odo mi.</p>
+              {/* <p>A Dashboard built using React Components, Material UI and </p> */}
+              <p>Empowering data-driven decisions, my project leverages REACT, APIs, 
+                and Material UI for seamless user experience and stunning data visualization.</p>
             </div>
           </div>
-          <div className="project-1 img-hover">
-            <a className="img-2 " href="https://lolubolarinwa.github.io/weather-app-project-/" />
+
+          <div className="project-1">
+            <a className="img-2 img-p " href="https://lolubolarinwa.github.io/weather-app-project-/" target="_blank"/>
             <div className="project-description">
-              <h3>Meh</h3>
+              <h3>API Forecaster Weather </h3>
               <p>
-                Lorem Ipsum jekajade lo si ode. Iwo na mo wipe mo feran re. Inu
-                mi ma tutu ti o ba sowipe o nbo wa si odo mi.
+              Using JavaScript, CSS, and Node.js. I created a dynamic web app with 
+              robust API integration, emphasizing efficient HTTP requests and data retrieval.
               </p>
             </div>
           </div>
+
           <div className="project-1">
-            <div className="project-img">{/* <img></img> */}</div>
+            <a className="img-3 img-p " href="https://lolubolarinwa.hashnode.dev/understanding-blockchain-consensus-mechanisms" target="_blank"/>
             <div className="project-description">
-              <h3 className="rebelh3">Meh</h3>
+              <h3>Consensu Algorithms</h3>
               <p>
-                Lorem Ipsum jekajade lo si ode. Iwo na mo wipe mo feran re. Inu
-                mi ma tutu ti o ba sowipe o nbo wa si odo mi.
-              </p>
-            </div>
-          </div>
-          <div className="project-1">
-            <div className="project-img">{/* <img></img> */}</div>
-            <div className="project-description">
-              <h3>Meh</h3>
-              <p>
-                Lorem Ipsum jekajade lo si ode. Iwo na mo wipe mo feran re. Inu
-                mi ma tutu ti o ba sowipe o nbo wa si odo mi.
+              My article delves into pivotal consensus mechanisms highlighting 
+              Ethereum's Proof of Work and Proof of Stake. This showcases my 
+              expertise in blockchain technology and technical communication skills.
               </p>
             </div>
           </div>
